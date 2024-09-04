2cf6e094674311ac8182a1f60be6d556c4214306
mailbau <muhammad.ismail.azmi@mail.ugm.ac.id>
2024-09-04 22:55:28 +0700

    feat: helper functions

---
+++ tetibot.py
@@ -0,0 +1,21 @@
+import webbrowser
+
+def curriculum_te():
+    url = "https://sarjana.jteti.ugm.ac.id/program-sarjana/program-studi-teknik-elektro/kurikulum-2021/"
+    webbrowser.open(url)
+
+def curriculum_ti():
+    url = "https://sarjana.jteti.ugm.ac.id/program-sarjana/program-studi-teknologi-informasi/kurikulum-2021/"
+    webbrowser.open(url)
+
+def curriculum_tb():
+    url = "https://sarjana.jteti.ugm.ac.id/program-sarjana/program-studi-teknik-biomedis/kurikulum-2021/"
+    webbrowser.open(url)
+
+def academic_documents():
+    url = "https://sarjana.jteti.ugm.ac.id/akademik/dokumen-akademik/"
+    webbrowser.open(url)
+
+def beasiswa():
+    url = "https://sarjana.jteti.ugm.ac.id/kemahasiswaan/peluang-mahasiswa/beasiswa/"
+    webbrowser.open(url)
